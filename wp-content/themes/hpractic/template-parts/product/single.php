080ef0e54de66afc0e64d446e241f6374f993b73
Yevhenii Rodkin <rodkin.yevhenii@gmail.com>
2021-03-19 11:00:12 +0200

    Added product content

--- wp-content/themes/hpractic/template-parts/product/single.php
+++ wp-content/themes/hpractic/template-parts/product/single.php
@@ -58,7 +58,8 @@ $breadcrumbs = new Breadcrumbs($id);
                             <div class="product__info-item">
                                 <div class="product__info-head">
                                     <svg class="icon">
-                                        <use xlink:href="<?php echo SRC_URI; ?>img/icons-sprite.svg#icon-credit-cards"></use>
+                                        <use xlink:href="<?php
+                                        echo SRC_URI; ?>img/icons-sprite.svg#icon-credit-cards"></use>
                                     </svg>
                                     <span><?php _e('Оплата', 'Hpractice'); ?></span>
                                 </div>
@@ -82,11 +83,13 @@ $breadcrumbs = new Breadcrumbs($id);
                 <div class="container">
                     <div class="tabs__nav-inner">
                         <div class="tabs__nav-item" data-tab="description">
-                            <span>Описание</span>
+                            <span><?php _e('Описание', 'Hpractice'); ?></span>
                         </div>
-                        <div class="tabs__nav-item" data-tab="characteristic">
-                            <span>Характеристики</span>
-                        </div>
+                        <?php if (!empty($product->getCharacteristic())) : ?>
+                            <div class="tabs__nav-item" data-tab="characteristic">
+                                <span><?php _e('Характеристики', 'Hpractice'); ?></span>
+                            </div>
+                        <?php endif; ?>
                     </div>
                 </div>
             </div>
@@ -94,109 +97,25 @@ $breadcrumbs = new Breadcrumbs($id);
                 <div class="container">
                     <div class="tabs__items-inner">
                         <div class="tabs__items-tab" data-tab="description">
-                            <p>
-                                Стол изготовлен из прозрачного материала - оргстекла. Форма столешницы прямоугольная.
-                                Профиль стола имеет о-образный вид. Скругления стола на углах плавные,
-                                легко поддаются уборке. Внизу имеется полка Для увеличения нагружаемой
-                                способности стола добавляем к конструкции усиливающие пластины под столешницу
-                                в виде арок.
-                                Внизу имеются 4 ножек, которые имеют круглую форму. Высота ножек 50 мм.
-                                Дополнительные опции:
-                            </p>
-                            <ul>
-                                <li>
-                                    На указанную часть изделия наносим желаемый рисунок, логотип.
-                                    Изображение может быть нанесено двумя способами: УФ-печать либо лазерная
-                                    гравировка
-                                </li>
-                                <li>
-                                    Придаем матовость прозрачным деталям
-                                </li>
-                            </ul>
-                            <h4>Комплект поставки:</h4>
-                            <ul>
-                                <li>
-                                    Стол
-                                </li>
-                                <li>
-                                    Компания "Practice House" является производителем продукции из оргстекла
-                                    и прочих пластиков. Изготавливаем товары согласно указанным габаритными
-                                    размерами и конфигурации
-                                </li>
-                            </ul>
-                            <figure class="table-container">
-                                <table>
-                                    <thead>
-                                    <tr>
-                                        <td>Столбец 1</td>
-                                        <td>Столбец 2</td>
-                                        <td>Столбец 3</td>
-                                        <td>Столбец 4</td>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    <tr>
-                                        <td>Ячейка 1</td>
-                                        <td>Ячейка 2</td>
-                                        <td>Ячейка 3</td>
-                                        <td>Ячейка 4</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Ячейка 1</td>
-                                        <td>Ячейка 2</td>
-                                        <td>Ячейка 3</td>
-                                        <td>Ячейка 4</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Ячейка 1</td>
-                                        <td>Ячейка 2</td>
-                                        <td>Ячейка 3</td>
-                                        <td>Ячейка 4</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Ячейка 1</td>
-                                        <td>Ячейка 2</td>
-                                        <td>Ячейка 3</td>
-                                        <td>Ячейка 4</td>
-                                    </tr>
-                                    </tbody>
-                                </table>
-                                <figcaption>Пример названия таблицы</figcaption>
-                            </figure>
-                            <p>
-                                Наша компания готова изготовить продукцию в необходимых вам габаритных
-                                размерах, с оптимальной комплектацией и в наиболее подходящем цветовом
-                                решении. Дополнительно на элементы конструкции наносим изображение с
-                                помощью цветной УФ печати, лазерной гравировки либо самоклеящейся пленки
-                                с изображением.
-                            </p>
+                            <?php the_content(); ?>
                         </div>
-                        <div class="tabs__items-tab" data-tab="characteristic">
-                            <h4>Габаритные размеры столика</h4>
-                            <figure class="table-container">
-                                <table>
-                                    <tbody>
-                                    <tr>
-                                        <td>Длина столика</td>
-                                        <td>800 мм</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Глубина столика</td>
-                                        <td>400 мм</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Высота столика</td>
-                                        <td>450 мм</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Толщина</td>
-                                        <td>10 мм</td>
-                                    </tr>
-                                    </tbody>
-                                </table>
-                                <figcaption>Пример названия таблицы</figcaption>
-                            </figure>
-                        </div>
+                        <?php if (!empty($product->getCharacteristic())) : ?>
+                            <div class="tabs__items-tab" data-tab="characteristic">
+                                <figure class="table-container">
+                                    <table>
+                                        <tbody>
+                                            <?php foreach ($product->getCharacteristic() as $item) : ?>
+                                                <tr>
+                                                    <td><?php echo $item['label']; ?></td>
+                                                    <td><?php echo $item['value']; ?></td>
+                                                </tr>
+                                            <?php endforeach; ?>
+                                        </tbody>
+                                    </table>
+                                    <figcaption><?php _e('Характеристики изделия', 'Hpractice'); ?></figcaption>
+                                </figure>
+                            </div>
+                        <?php endif; ?>
                     </div>
                 </div>
             </div>
