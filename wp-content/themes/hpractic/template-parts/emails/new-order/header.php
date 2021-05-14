76398d09790e8b9e3c256fd55a09b2018676e680
Yevhenii Rodkin <rodkin.yevhenii@gmail.com>
2021-05-14 16:33:00 +0300

    Fixed header

--- wp-content/themes/hpractic/template-parts/emails/new-order/header.php
+++ wp-content/themes/hpractic/template-parts/emails/new-order/header.php
@@ -83,54 +83,6 @@ use Hpr\Service\Helpers\Helpers;
             </div>
             <!--[if (mso)|(IE)]></td></tr></table><![endif]-->
             <!--[if (mso)|(IE)]></td>
-            <td align="center" width="175"
-                style="background-color:#f2f5f8;width:175px; border-top: 0px solid transparent; border-left: 0px solid transparent; border-bottom: 0px solid transparent; border-right: 0px solid transparent;"
-                valign="top">
-                <table width="100%" cellpadding="0" cellspacing="0" border="0">
-                    <tr>
-                        <td style="padding-right: 0px; padding-left: 0px; padding-top:20px; padding-bottom:5px;">
-            <![endif]-->
-            <div class="col num3"
-                 style="display: table-cell; vertical-align: top; max-width: 320px; min-width: 174px; width: 175px;">
-                <div class="col_cont" style="width:100% !important;">
-                    <!--[if (!mso)&(!IE)]><!-->
-                    <div
-                        style="border-top:0px solid transparent; border-left:0px solid transparent; border-bottom:0px solid transparent; border-right:0px solid transparent; padding-top:20px; padding-bottom:5px; padding-right: 0px; padding-left: 0px;">
-                        <!--<![endif]-->
-                        <div class="mobile_hide">
-                            <table border="0" cellpadding="0" cellspacing="0" class="divider"
-                                   role="presentation"
-                                   style="table-layout: fixed; vertical-align: top; border-spacing: 0; border-collapse: collapse; mso-table-lspace: 0pt; mso-table-rspace: 0pt; min-width: 100%; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%;"
-                                   valign="top" width="100%">
-                                <tbody>
-                                <tr style="vertical-align: top;" valign="top">
-                                    <td class="divider_inner"
-                                        style="word-break: break-word; vertical-align: top; min-width: 100%; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%; padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px;"
-                                        valign="top">
-                                        <table align="center" border="0" cellpadding="0" cellspacing="0"
-                                               class="divider_content" height="0" role="presentation"
-                                               style="table-layout: fixed; vertical-align: top; border-spacing: 0; border-collapse: collapse; mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-top: 0px solid transparent; height: 0px; width: 100%;"
-                                               valign="top" width="100%">
-                                            <tbody>
-                                            <tr style="vertical-align: top;" valign="top">
-                                                <td height="0"
-                                                    style="word-break: break-word; vertical-align: top; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%;"
-                                                    valign="top"><span></span></td>
-                                            </tr>
-                                            </tbody>
-                                        </table>
-                                    </td>
-                                </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                        <!--[if (!mso)&(!IE)]><!-->
-                    </div>
-                    <!--<![endif]-->
-                </div>
-            </div>
-            <!--[if (mso)|(IE)]></td></tr></table><![endif]-->
-            <!--[if (mso)|(IE)]></td>
             <td align="center" width="350"
                 style="background-color:#f2f5f8;width:350px; border-top: 0px solid transparent; border-left: 0px solid transparent; border-bottom: 0px solid transparent; border-right: 0px solid transparent;"
                 valign="top">
